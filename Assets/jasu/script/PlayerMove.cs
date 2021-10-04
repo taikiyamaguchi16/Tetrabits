2f1b7f4271d1ad5e2bf9f0c6ceafd42e67ae16ab
KentaOishi <justicekenta@i.softbank.jp>
2021-10-04 15:04:50 +0900

    [add]仮player作成

---
+++ Assets/jasu/script/PlayerMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    Camera camera = null;
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    float upForce = 200f;    //ジャンプ力
+
+    private bool isGround;          // 着地判定
+
+    [SerializeField]
+    float moveSpd = 0.03f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += camera.transform.forward * moveSpd;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.position -= camera.transform.forward * moveSpd;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.position += camera.transform.right * moveSpd;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.position -= camera.transform.right * moveSpd;
+        }
+
+        if (isGround == true)//着地しているとき
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                isGround = false;
+                rb.AddForce(new Vector3(0, upForce, 0));
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other) //地面に触れた時の処理
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
+    }
+}
---
+++ Assets/jasu/script/Zenmai.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zenmai : MonoBehaviour
+{
+    public int maxZenmaiPower = 100;
+    
+    public int decreasePS = 1;
+
+    public int zenmaiPower;
+
+    float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zenmaiPower = maxZenmaiPower;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 1 && zenmaiPower > 0)
+        {
+            timer = 0f;
+            zenmaiPower -= decreasePS;
+        }
+    }
+}
