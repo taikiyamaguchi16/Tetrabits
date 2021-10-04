0f42111e9b9e1b442ae209f03502b45363ddef03
hopehopehope <hope03282000@icloud.com>
2021-10-04 16:24:16 +0900

    [add] tetraInputの作成 ※プロジェクト設定とレイヤーいじってます

---
+++ Assets/tagami/Scripts/TetraInput/TetraPad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetraPad : MonoBehaviour
+{
+    [Header("Reference")]
+    [SerializeField] TetraPadBody tetraPadBody;
+
+    Vector2 padVector;
+
+    private void Update()
+    {
+        //リストから合算ベクトルを作成
+        padVector = Vector2.zero;
+        foreach (var obj in tetraPadBody.padOnList)
+        {
+            var localVec = obj.transform.position - transform.position;
+            localVec.Normalize();
+            padVector.x += localVec.x;
+            padVector.y += localVec.z;
+        }
+    }
+
+    public Vector2 GetVector(){ return padVector; }
+}
