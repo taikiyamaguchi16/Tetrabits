9704a90e0faa279b8fa6d326f32d9d699d6b4e9d
tai <taikitpmiita@icloud.com>
2021-10-08 09:45:31 +0900

    [add]通信系処理の追加

---
+++ Assets/yamaguchi/Script/SampleScene.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using ExitGames.Client.Photon;
+
+
+public class SampleScene : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        //PhotonNetwork.NickName = "Player";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        //PhotonNetwork.Instantiate("Avatar", position, Quaternion.identity);
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.CurrentRoom.SetStartTime(PhotonNetwork.ServerTimestamp);
+        }
+    }
+}
