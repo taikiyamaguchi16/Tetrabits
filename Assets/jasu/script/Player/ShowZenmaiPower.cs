50369e1cb6976d2fb44b92c67f686602589db9d9
KentaOishi <justicekenta@i.softbank.jp>
2021-10-11 17:20:24 +0900

    [update]プレイヤーのジャンプ改善、ゼンマイのUIの色を割合で変更

---
+++ Assets/jasu/script/Player/ShowZenmaiPower.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct ColorInRatio
+{
+    [Tooltip("割合(0~1)")] public float ratio;
+    [Tooltip("UIカラー")] public Color color;
+}
+
+public class ShowZenmaiPower : MonoBehaviour
+{
+    [SerializeField]
+    Zenmai zenmai;
+
+    [SerializeField]
+    Slider slider;
+
+    [SerializeField]
+    List<ColorInRatio> colorInRatios = new List<ColorInRatio>();
+
+    [SerializeField]
+    Image fillImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.maxValue = zenmai.maxZenmaiPower;
+        slider.value = zenmai.maxZenmaiPower;
+
+        // ソート
+        for (int i = 0; i < colorInRatios.Count; i++)
+        {
+            for (int j = i + 1; j < colorInRatios.Count; j++)
+            {
+                if (colorInRatios[i].ratio < colorInRatios[j].ratio)
+                {
+                    ColorInRatio tmp = colorInRatios[i];
+                    colorInRatios[i] = colorInRatios[j];
+                    colorInRatios[j] = tmp;
+                }
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        slider.value = zenmai.zenmaiPower;  // スライダーに値を適用
+
+        // ゼンマイパワーからカラー決定
+        float ratio = zenmai.zenmaiPower / zenmai.maxZenmaiPower;
+        foreach (var colorInRatio in colorInRatios)
+        {
+            if (ratio <= colorInRatio.ratio)
+                fillImage.color = colorInRatio.color;
+        }
+    }
+}
---
+++ Assets/jasu/script/general/TriggerSensor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// トリガー内のオブジェクトの数を数える
+public class TriggerSensor : MonoBehaviour
+{
+    int inTriggerNum;   // オブジェクトのカウント
+
+    bool existInTrigger = false; // トリガー内に何かしらのオブジェクトがあればtrue;
+
+    private void Start()
+    {
+        inTriggerNum = 0;
+    }
+
+    private void Update()
+    {
+        if(inTriggerNum > 0)
+        {
+            existInTrigger = true;
+        }
+        else
+        {
+            existInTrigger = false;
+            inTriggerNum = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inTriggerNum++;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTriggerNum--;
+    }
+
+    public int GetInTriggerNum()
+    {
+        return inTriggerNum;
+    }
+
+    public bool GetExistInTrigger()
+    {
+        return existInTrigger;
+    }
+}
