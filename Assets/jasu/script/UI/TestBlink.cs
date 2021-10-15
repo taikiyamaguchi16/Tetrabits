1e14bf2d1ea5d7f66226d4274ddbee8873e32b95
KentaOishi <justicekenta@i.softbank.jp>
2021-10-15 12:00:24 +0900

    [add]タイトル作成

---
+++ Assets/jasu/script/UI/TestBlink.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestBlink : MonoBehaviour
+{
+    [SerializeField]
+    Text text;
+
+    [SerializeField]
+    float changeValue = 0.1f;
+
+    bool rise = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color color = text.color;
+        if (rise)
+        {
+            color.a -= changeValue;
+            if (color.a <= 0f)
+                rise = false;
+        }
+        else
+        {
+            color.a += changeValue;
+            if (color.a >= 1f)
+                rise = true;
+        }
+        text.color = color;
+    }
+}
