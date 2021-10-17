90c3a3365c9771bbcdd32f0b953a9b7ad9e66b85
hopehopehope <hope03282000@icloud.com>
2021-10-17 21:08:47 +0900

    [update] GameInGame用のManager仕様を単純化しました

--- Assets/tagami/Scripts/GameInGame/Monitor/RenderTextureWindowSizeScaler.cs
+++ Assets/tagami/Scripts/GameInGame/Monitor/RenderTextureWindowSizeScaler.cs
@@ -10,8 +10,9 @@ public class RenderTextureWindowSizeScaler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        renderTextureCamera = GetComponent<Camera>();
-        renderTextureCamera.targetTexture.width = Screen.width;
-        renderTextureCamera.targetTexture.height = Screen.height;
+        //renderTextureCamera = GetComponent<Camera>();
+        //renderTextureCamera.targetTexture.width = Screen.width;
+        //renderTextureCamera.targetTexture.height = Screen.height;
+        Debug.LogError("この機能は一時的に制限されています。使用しないでください。 2021/10/17");
     }
 }
