16b92a8156c7218d5b52b348d8976cb16de5ad6d
KentaOishi <justicekenta@i.softbank.jp>
2021-10-18 17:15:23 +0900

    [update]spawner改良、使ってた人はアタッチとか設定し直してっちょ

---
+++ Assets/jasu/script/general/DestroyObjectCpt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectCpt : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> gameObjectList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DestroyObject()
+    {
+        for(int i= 0; i < gameObjectList.Count; i++)
+        {
+            Destroy(gameObjectList[i]);
+        }
+        Destroy(this);
+    }
+}
