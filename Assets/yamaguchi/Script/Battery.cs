d41fa5a3611fa902ce85a57771486b32b0de822e
tai <taikitpmiita@icloud.com>
2021-10-21 23:46:15 +0900

    [update]カセットの変更

--- Assets/yamaguchi/Script/Battery.cs
+++ Assets/yamaguchi/Script/Battery.cs
@@ -14,22 +14,31 @@ public class Battery : MonoBehaviourPunCallbacks,IPlayerAction
     public bool isOwned;
     [SerializeField]
     private int priority;
+    //電池残量
+    private float level;
 
-    private BatteryHolder ownerSc;
+    private ItemPocket ownerSc;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<BoxCollider>();
-        isOwned = false;
+        isOwned = false;       
     }
 
     public void StartPlayerAction(PlayerActionDesc _desc)
     {
-        if (!isOwned)
-            PickUp(_desc);
-        else
-            Dump(_desc);
+        if (photonView.IsMine)
+        {
+            if (!isOwned)
+            {
+                PickUp(_desc.playerObj);
+                //photonView.RPC(nameof(PickUp), RpcTarget.All);
+            }
+            else
+                //photonView.RPC(nameof(Dump), RpcTarget.All, _desc.playerObj);
+            Dump(_desc.playerObj);
+        }
     }
     public void EndPlayerAction(PlayerActionDesc _desc) { }
     public int GetPriority()
@@ -37,26 +46,35 @@ public class Battery : MonoBehaviourPunCallbacks,IPlayerAction
         return priority;
     }
 
-    public void Dump(PlayerActionDesc _desc)
+    [PunRPC]
+    public void Dump(GameObject _obj)
     {
-        if (_desc.playerObj == ownerSc.gameObject)
+        if (_obj == ownerSc.gameObject)
         {
-            ownerSc.SetBattery(null);
+            ownerSc.SetItem(null);
             rb.isKinematic = false;
             col.enabled = true;
             this.transform.parent = null;
             isOwned = false;
+            priority = 40;
         }
     }
 
-    private void PickUp(PlayerActionDesc _desc)
+    [PunRPC]
+    public void PickUp(GameObject _obj)
     {
-        ownerSc = _desc.playerObj.GetComponent<BatteryHolder>();
-        ownerSc.SetBattery(this.gameObject);
+        priority = 100;
+        ownerSc = _obj.GetComponent<ItemPocket>();
+        ownerSc.SetItem(this.gameObject);
         rb.isKinematic = true;
         col.enabled = false;
-        this.transform.parent = _desc.playerObj.transform;
+        this.transform.parent = _obj.transform;
         //保有状態に切り替え
         isOwned = true;
+    }
+
+    public float GetLevel()
+    {
+        return level;
     }
 }
---
+++ Assets/yamaguchi/Script/CassettHolder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CassetteHolder : MonoBehaviour, IPlayerAction
+{
+    private Cassette ownCassette;
+
+    private ItemPocket pocket;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pocket = GetComponent<ItemPocket>();
+    }
+
+    public void StartPlayerAction(PlayerActionDesc _desc)
+    {
+        ItemPocket otherPocket = _desc.playerObj.GetComponent<ItemPocket>();
+
+        if (otherPocket.GetItem() != null)
+        {
+            ownCassette = otherPocket.GetItem().GetComponent<Cassette>();
+            //カセットだった場合セット
+            if (ownCassette != null)
+            {
+                //プレイヤーのアイテムを取得してセット
+                pocket.SetItem(otherPocket.GetItem());
+                ownCassette.Dump(_desc.playerObj);
+            }
+        }
+    }
+    public void EndPlayerAction(PlayerActionDesc _desc) { }
+    public int GetPriority()
+    {
+        return 150;
+    }
+}
