309694e962f9d6d872ab9adb71b977656a8e4d97
hopehopehope <hope03282000@icloud.com>
2021-10-25 17:31:41 +0900

    [add,tag] 冷却オブジェクトを作成

---
+++ Assets/tagami/Scripts/Monitor/CoolerRotater.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolerRotater : MonoBehaviour
+{
+    Transform rotateTarget;
+
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int xinputIndex = 0;
+
+        if(XInputManager.GetButtonPress(xinputIndex,XButtonType.LThumbStickLeft))
+        {
+            //rotateTarget.rotation *= Quaternion.AngleAxis(,);
+        }
+        if (XInputManager.GetButtonPress(xinputIndex, XButtonType.LThumbStickRight))
+        {
+
+        }
+        if (XInputManager.GetButtonPress(xinputIndex, XButtonType.LThumbStickUp))
+        {
+
+        }
+        if (XInputManager.GetButtonPress(xinputIndex, XButtonType.LThumbStickDown))
+        {
+
+        }
+
+    }
+}
