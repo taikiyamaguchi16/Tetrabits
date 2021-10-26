5786202fbb4ec1c8063c818c60153ade94e87717
tai <taikitpmiita@icloud.com>
2021-10-26 14:04:52 +0900

    [update]テスト用のpush

--- Assets/yamaguchi/Script/Item/Battery.cs
+++ Assets/yamaguchi/Script/Item/Battery.cs
@@ -49,6 +49,7 @@ public class Battery : MonoBehaviourPunCallbacks,IPlayerAction
 
     private void Update()
     {
+        Debug.Log(photonView.ViewID);
         level -= powerConsumption * Time.deltaTime;
         if (level < 0)
             level = 0f;
@@ -78,7 +79,6 @@ public class Battery : MonoBehaviourPunCallbacks,IPlayerAction
     {
         //持たれたとき用の角度
         this.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
-
         priority = 100;
         ownerSc = _obj.GetComponent<ItemPocket>();
         ownerSc.SetItem(this.gameObject);
