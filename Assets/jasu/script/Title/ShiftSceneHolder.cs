644c760c502f90f2f4ef9bf38551f878edba2a11
KentaOishi <justicekenta@i.softbank.jp>
2021-10-27 00:06:48 +0900

    [update]スクリプトの名前変更

--- Assets/jasu/script/Title/ShiftSceneHolder.cs
+++ Assets/jasu/script/Title/ShiftSceneHolder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneHolder : MonoBehaviour
+public class ShiftSceneHolder : MonoBehaviour
 {
     [SerializeField]
     SceneObject sceneObject;
