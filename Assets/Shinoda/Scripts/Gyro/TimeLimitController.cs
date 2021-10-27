d4358e9fcc940fef64c8898d7922145b3b0afdb8
nona-nano-na <ohs80645@gmail.com>
2021-10-27 15:06:10 +0900

    [update]gyrogame
    一連の流れ。タイムUI。

--- Assets/Shinoda/Scripts/Gyro/TimeLimitController.cs
+++ Assets/Shinoda/Scripts/Gyro/TimeLimitController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeLimitController : MonoBehaviour
 {
@@ -9,13 +10,15 @@ public class TimeLimitController : MonoBehaviour
     [SerializeField,Tooltip("制限時間")] float limit;
     [SerializeField, Tooltip("ダメージ量")] float damage;
 
+    Text timeText;
+
     float timeCount = 0f;
     float slipCount = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeText = transform.Find("TimeText").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -39,5 +42,7 @@ public class TimeLimitController : MonoBehaviour
                 timeCount = 0;
             }
         }
+
+        timeText.text = (limit - timeCount).ToString("f0");
     }
 }
