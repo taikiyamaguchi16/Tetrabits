7bc415fc3b40bbd53b09302f2ad017eaad6e6e59
hopehopehope <hope03282000@icloud.com>
2021-10-27 17:23:30 +0900

    [update] GameMainSceneの更新、TetraInputのモデル導入

---
+++ Assets/tagami/Scripts/TetraInput/ButtonBodyCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBodyCollider : MonoBehaviour
+{
+    public int collisionNum { private set; get; } = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collisionNum++;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collisionNum--;
+    }
+}
