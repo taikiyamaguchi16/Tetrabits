c9071ceec388ec07503839b45204d30b54765729
KentaOishi <justicekenta@i.softbank.jp>
2021-11-01 17:01:12 +0900

    [update]CRTの明るさ調整機能追加

--- Assets/jasu/script/ForShader/CRT.cs
+++ Assets/jasu/script/ForShader/CRT.cs
@@ -51,6 +51,10 @@ public class CRT : MonoBehaviour
     float scanLineSpeed = 0.5f;
     public float ScanLineSpeed { get { return scanLineSpeed; } set { scanLineSpeed = value; } }
 
+    [SerializeField]
+    float darknessAtEdge = 1.2f;
+    public float DarknessAtEdge { get { return darknessAtEdge; } set { darknessAtEdge = value; } }
+
     // Use this for initialization
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
