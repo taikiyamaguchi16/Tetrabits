f76320d0c49fb485cd7a6d8d80f94e07f5ad201a
SatoSho <aries_3042@i.softbank.jp>
2021-11-02 13:28:28 +0900

    [add]タイトル仮完成品

---
+++ Assets/sato/Script/UI/ButtonParent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ButtonParent : DOManager
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CallMove()
+    {
+        // 現在位置から設定した位置に移動
+        transform.DOLocalMove(moveRange, moveTime).SetRelative(true);
+    }
+}
---
+++ Assets/sato/Script/UI/TitleLogo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TitleLogo : DOManager
+{
+    [SerializeField]
+    GameObject buttonParent;
+
+    Button[] selectButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // アタッチされたキャンバスの子のボタンコンポーネント取得
+        selectButton = buttonParent.GetComponentsInChildren<Button>();
+
+        Scaling();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Scaling()
+    {
+        if (scaleFlag)
+        {
+            // スケールを指定したアニメーションをさせながら変更
+            transform.DOScale(scaleRange, scaleTime).SetEase(easeTypes).SetLoops(loopTimes, loopTypes).OnComplete(() =>
+            {
+                for(int i = 0; i < selectButton.Length; i++)
+                {
+                    selectButton[i].interactable = true;
+                }
+                buttonParent.GetComponent<ButtonParent>().CallMove();
+            });
+        }
+    }
+}
