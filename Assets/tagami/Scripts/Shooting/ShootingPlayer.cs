d501605437ccfc97386bee6e93d0db6e0693f856
hopehopehope <hope03282000@icloud.com>
2021-11-05 12:41:03 +0900

    [update,buildSetting] シューティングゲーム作りかけ

---
+++ Assets/tagami/Scripts/Shooting/ShootingPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShootingPlayer : MonoBehaviour
+{
+    Rigidbody2D myRb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRb2D.velocity = TetraInput.sTetraPad.GetVector();
+    }
+}
