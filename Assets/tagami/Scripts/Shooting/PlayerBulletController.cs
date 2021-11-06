3f378d7bd074064ac8295cae3d3d875e5d9df641
hopehopehope <hope03282000@icloud.com>
2021-11-07 00:53:09 +0900

    [update,tag] 連なり敵と固定砲台敵、および敵の生成方法の基盤作成　

--- Assets/tagami/Scripts/Shooting/PlayerBulletController.cs
+++ Assets/tagami/Scripts/Shooting/PlayerBulletController.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerBulletController : MonoBehaviour
 {
-    [SerializeField] float bulletSpeed = 1.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        GetComponent<Rigidbody2D>().velocity = transform.right * bulletSpeed;
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
