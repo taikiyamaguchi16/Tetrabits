f338620d6c02d151d4141d9e8805b02564b19996
tai <taikitpmiita@icloud.com>
2021-11-08 18:43:04 +0900

    [update]カメラを引く処理を追加

--- Assets/yamaguchi/Script/OnlineWaitRoomSetUper.cs
+++ Assets/yamaguchi/Script/OnlineWaitRoomSetUper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class OnlineWaitRoomSetUper : MonoBehaviour
+public class OnlineWaitRoomSetUper : MonoBehaviourPunCallbacks
 {
 
     // Start is called before the first frame update
@@ -11,22 +11,27 @@ public class OnlineWaitRoomSetUper : MonoBehaviour
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            //カセット表示オン
-            //とりあえずFindでテスト
-            var cassetHolderObj = GameObject.Find("CassetHolder");
-            if (!cassetHolderObj)
-            {
-                cassetHolderObj = GameObject.Find("cassette_socket2");
-            }
-
-            var batterySpoawnerObj = GameObject.Find("BatterySpawner");
-            cassetHolderObj.GetComponent<CassetteManager>().AppearAllCassette();
-
-            batterySpoawnerObj.GetComponent<BatterySpowner>().StartSpawn();
-            VirtualCameraManager.OnlyActive(1);
-
+            photonView.RPC(nameof(CallStartGameOnline), RpcTarget.All);
             //ルームを入室不可に
             PhotonNetwork.CurrentRoom.IsOpen = false;
         }
+    }
+
+    [PunRPC]
+    public void CallStartGameOnline()
+    {
+        //カセット表示オン
+        //とりあえずFindでテスト
+        var cassetHolderObj = GameObject.Find("CassetHolder");
+        if (!cassetHolderObj)
+        {
+            cassetHolderObj = GameObject.Find("cassette_socket2");
+        }
+
+        var batterySpoawnerObj = GameObject.Find("BatterySpawner");
+        cassetHolderObj.GetComponent<CassetteManager>().AppearAllCassette();
+
+        batterySpoawnerObj.GetComponent<BatterySpowner>().StartSpawn();
+        VirtualCameraManager.OnlyActive(1);
     }
 }
