c7adfd189497e1701a4e8ee61f938e57889796e2
tai <taikitpmiita@icloud.com>
2021-11-08 17:34:45 +0900

    [update]if分追加

--- Assets/yamaguchi/Script/OnlineWaitRoomSetUper.cs
+++ Assets/yamaguchi/Script/OnlineWaitRoomSetUper.cs
@@ -9,21 +9,24 @@ public class OnlineWaitRoomSetUper : MonoBehaviour
     // Start is called before the first frame update
     public void OnlineGameStart()
     {
-        //カセット表示オン
-        //とりあえずFindでテスト
-        var cassetHolderObj = GameObject.Find("CassetHolder");
-        if (!cassetHolderObj)
+        if (PhotonNetwork.IsMasterClient)
         {
-            cassetHolderObj = GameObject.Find("cassette_socket2");
+            //カセット表示オン
+            //とりあえずFindでテスト
+            var cassetHolderObj = GameObject.Find("CassetHolder");
+            if (!cassetHolderObj)
+            {
+                cassetHolderObj = GameObject.Find("cassette_socket2");
+            }
+
+            var batterySpoawnerObj = GameObject.Find("BatterySpawner");
+            cassetHolderObj.GetComponent<CassetteManager>().AppearAllCassette();
+
+            batterySpoawnerObj.GetComponent<BatterySpowner>().StartSpawn();
+            VirtualCameraManager.OnlyActive(1);
+
+            //ルームを入室不可に
+            PhotonNetwork.CurrentRoom.IsOpen = false;
         }
-
-        var batterySpoawnerObj = GameObject.Find("BatterySpawner");
-        cassetHolderObj.GetComponent<CassetteManager>().AppearAllCassette();
-
-        batterySpoawnerObj.GetComponent<BatterySpowner>().StartSpawn();
-        VirtualCameraManager.OnlyActive(1);
-
-        //ルームを入室不可に
-        PhotonNetwork.CurrentRoom.IsOpen = false;
     }
 }
