c4249aa9e3a69b7525afc004723092c3f1f8e345
hopehopehope <hope03282000@icloud.com>
2021-11-11 00:09:33 +0900

    [update] ボム、惑星敵、シューティング用汎用クラスの作成

--- Assets/tagami/Scripts/Shooting/Enemy/OnDamagedDestroy.cs
+++ Assets/tagami/Scripts/Shooting/Enemy/OnDamagedDestroy.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class OnDamagedDestroy : MonoBehaviour, IShootingEnemy
 {
+    [SerializeField] int hp = 1;
+
     [Header("Option")]
     [SerializeField] List<GameObject> additionalDestroyObjects;
 
     public void OnDamaged()
     {
-        Destroy(gameObject);
-
-        foreach(var obj in additionalDestroyObjects)
+        hp--;
+        if (hp <= 0)
         {
-            Destroy(obj);
+            Destroy(gameObject);
+
+            foreach (var obj in additionalDestroyObjects)
+            {
+                Destroy(obj);
+            }
         }
     }
 }
