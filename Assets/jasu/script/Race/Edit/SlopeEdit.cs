24341aa5f7b5fb01e21ec0a145d3f1aa5d3d0d47
KentaOishi <justicekenta@i.softbank.jp>
2021-11-12 00:35:21 +0900

    [update]ステージ編集機能追加

--- Assets/jasu/script/Race/Edit/SlopeEdit.cs
+++ Assets/jasu/script/Race/Edit/SlopeEdit.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+[ExecuteAlways]
 public class SlopeEdit : MonoBehaviour
 {
     [SerializeField]
@@ -48,84 +48,87 @@ public class SlopeEdit : MonoBehaviour
 
     private void OnEnable()
     {
-        if (road != null &&
-             upSlope != null && upSlopeCol != null &&
-             downSlope != null && downSlopeCol != null)
+        if (!Application.isPlaying)
         {
-            // Roadの長さ、高さと位置をセット
-            Vector3 scale = road.transform.localScale;
-            scale.y = height;
-            scale.z = roadLength;
-            road.transform.localScale = scale;
-            Vector3 pos = road.transform.localPosition;
-            pos.y = height / 2;
-            road.transform.localPosition = pos;
-
-            // 坂の角度
-            Vector3 angle = upSlope.transform.localRotation.eulerAngles;
-            angle.x = -upSlopeAngle;
-            upSlope.transform.localRotation = Quaternion.Euler(angle);
-
-            angle = downSlope.transform.localRotation.eulerAngles;
-            angle.x = downSlopeAngle;
-            downSlope.transform.localRotation = Quaternion.Euler(angle);
-            
-            // 坂の長さ
-            scale = upSlopeCol.transform.localScale;
-            scale.z = height / Mathf.Sin(upSlopeAngle * Mathf.Deg2Rad);
-            upSlopeCol.transform.localScale = scale;
-
-            scale = downSlopeCol.transform.localScale;
-            scale.z = height / Mathf.Sin(downSlopeAngle * Mathf.Deg2Rad);
-            downSlopeCol.transform.localScale = scale;
-
-            // 坂の位置
-            pos = upSlope.transform.localPosition;
-            pos.y = height / 2;
-            pos.z = -((upSlopeCol.transform.localScale.z / 2) * Mathf.Cos(upSlopeAngle * Mathf.Deg2Rad) + roadLength / 2);
-            upSlope.transform.localPosition = pos;
-
-            pos = downSlope.transform.localPosition;
-            pos.y = height / 2;
-            pos.z = (downSlopeCol.transform.localScale.z / 2) * Mathf.Cos(downSlopeAngle * Mathf.Deg2Rad) + roadLength / 2;
-            downSlope.transform.localPosition = pos;
-
-            // スプライト対応
-            if(upSlopeSprite != null &&
-                downSlopeSprite != null)
+            if (road != null &&
+                 upSlope != null && upSlopeCol != null &&
+                 downSlope != null && downSlopeCol != null)
             {
-                // スケール
-                scale = upSlopeSprite.transform.localScale;
-                scale.x = Mathf.Cos(upSlopeAngle * Mathf.Deg2Rad) * upSlopeCol.transform.localScale.z;
+                // Roadの長さ、高さと位置をセット
+                Vector3 scale = road.transform.localScale;
                 scale.y = height;
-                upSlopeSprite.transform.localScale = scale;
+                scale.z = roadLength;
+                road.transform.localScale = scale;
+                Vector3 pos = road.transform.localPosition;
+                pos.y = height / 2;
+                road.transform.localPosition = pos;
 
-                scale = downSlopeSprite.transform.localScale;
-                scale.x = Mathf.Cos(downSlopeAngle * Mathf.Deg2Rad) * downSlopeCol.transform.localScale.z;
-                scale.y = height;
-                downSlopeSprite.transform.localScale = scale;
+                // 坂の角度
+                Vector3 angle = upSlope.transform.localRotation.eulerAngles;
+                angle.x = -upSlopeAngle;
+                upSlope.transform.localRotation = Quaternion.Euler(angle);
 
-                // 位置
-                pos = upSlopeSprite.transform.localPosition;
+                angle = downSlope.transform.localRotation.eulerAngles;
+                angle.x = downSlopeAngle;
+                downSlope.transform.localRotation = Quaternion.Euler(angle);
+
+                // 坂の長さ
+                scale = upSlopeCol.transform.localScale;
+                scale.z = height / Mathf.Sin(upSlopeAngle * Mathf.Deg2Rad);
+                upSlopeCol.transform.localScale = scale;
+
+                scale = downSlopeCol.transform.localScale;
+                scale.z = height / Mathf.Sin(downSlopeAngle * Mathf.Deg2Rad);
+                downSlopeCol.transform.localScale = scale;
+
+                // 坂の位置
+                pos = upSlope.transform.localPosition;
                 pos.y = height / 2;
                 pos.z = -((upSlopeCol.transform.localScale.z / 2) * Mathf.Cos(upSlopeAngle * Mathf.Deg2Rad) + roadLength / 2);
-                upSlopeSprite.transform.localPosition = pos;
+                upSlope.transform.localPosition = pos;
 
-                pos = downSlopeSprite.transform.localPosition;
+                pos = downSlope.transform.localPosition;
                 pos.y = height / 2;
                 pos.z = (downSlopeCol.transform.localScale.z / 2) * Mathf.Cos(downSlopeAngle * Mathf.Deg2Rad) + roadLength / 2;
-                downSlopeSprite.transform.localPosition = pos;
-            }
+                downSlope.transform.localPosition = pos;
 
-            if(roadSprite != null &&
-                roadSpriteRenderer != null)
-            {
-                pos = roadSprite.transform.localPosition;
-                pos.y = height;
-                roadSprite.transform.localPosition = pos;
-                roadSpriteRenderer.size = new Vector2(roadLength / 5, roadSpriteRenderer.size.y);
-            }
+                // スプライト対応
+                if (upSlopeSprite != null &&
+                    downSlopeSprite != null)
+                {
+                    // スケール
+                    scale = upSlopeSprite.transform.localScale;
+                    scale.x = Mathf.Cos(upSlopeAngle * Mathf.Deg2Rad) * upSlopeCol.transform.localScale.z;
+                    scale.y = height;
+                    upSlopeSprite.transform.localScale = scale;
 
+                    scale = downSlopeSprite.transform.localScale;
+                    scale.x = Mathf.Cos(downSlopeAngle * Mathf.Deg2Rad) * downSlopeCol.transform.localScale.z;
+                    scale.y = height;
+                    downSlopeSprite.transform.localScale = scale;
+
+                    // 位置
+                    pos = upSlopeSprite.transform.localPosition;
+                    pos.y = height / 2;
+                    pos.z = -((upSlopeCol.transform.localScale.z / 2) * Mathf.Cos(upSlopeAngle * Mathf.Deg2Rad) + roadLength / 2);
+                    upSlopeSprite.transform.localPosition = pos;
+
+                    pos = downSlopeSprite.transform.localPosition;
+                    pos.y = height / 2;
+                    pos.z = (downSlopeCol.transform.localScale.z / 2) * Mathf.Cos(downSlopeAngle * Mathf.Deg2Rad) + roadLength / 2;
+                    downSlopeSprite.transform.localPosition = pos;
+                }
+
+                if (roadSprite != null &&
+                    roadSpriteRenderer != null)
+                {
+                    pos = roadSprite.transform.localPosition;
+                    pos.y = height;
+                    roadSprite.transform.localPosition = pos;
+                    roadSpriteRenderer.size = new Vector2(roadLength / 5, roadSpriteRenderer.size.y);
+                }
+
+            }
         }
     }
 }
