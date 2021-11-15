b30208ba490205703eedb1f68ee986fd3f06e426
hopehopehope <hope03282000@icloud.com>
2021-11-15 17:41:43 +0900

    [update] 階段モデルの導入、カメラ処理の導入を行いました

---
+++ Assets/tagami/Scripts/GameMain/Camera/VCameraSwitchSensor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//トリガーエンター条件でVCam変更
+[RequireComponent(typeof(Collider))]
+public class VCameraSwitchSensor : MonoBehaviour
+{
+    [SerializeField] int enterVCamIndex = -1;
+    [SerializeField] int exitVCamIndex = -1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enterVCamIndex >= 0)
+        {
+            VirtualCameraManager.OnlyActive(enterVCamIndex);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (exitVCamIndex >= 0)
+        {
+            VirtualCameraManager.OnlyActive(exitVCamIndex);
+        }
+    }
+}
