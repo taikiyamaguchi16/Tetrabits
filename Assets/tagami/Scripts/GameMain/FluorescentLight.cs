a077d3fad7c8b94bf9d5444a6d9700809bc2addb
hopehopehope <hope03282000@icloud.com>
2021-11-20 16:30:20 +0900

    [add] 蛍光灯っぽいライトの作成

---
+++ Assets/tagami/Scripts/GameMain/FluorescentLight.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class FluorescentLight : MonoBehaviour
+{
+   
+    [SerializeField] AnimationCurve intensityCurve;
+    [SerializeField] float flickeringSeconds = 1.0f;
+    float intensityMultiplier;
+    float flickeringTimer;
+    bool isFlickering;
+
+    Light myLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+        intensityMultiplier = myLight.intensity;
+
+        //StartFlickering();
+    }
+
+    private void OnEnable()
+    {
+        StartFlickering();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flickeringSeconds <= 0) return;
+
+        if (isFlickering)
+        {
+            flickeringTimer += Time.deltaTime;
+            if (flickeringTimer >= flickeringSeconds)
+            {
+                //終了
+                isFlickering = false;
+                flickeringTimer = 0.0f;
+                myLight.intensity = intensityMultiplier;
+            }
+            else
+            {
+                //明るさを変更する
+                myLight.intensity = intensityCurve.Evaluate(flickeringTimer / flickeringSeconds) * intensityMultiplier;
+            }         
+        }
+    }
+
+    void StartFlickering()
+    {
+        isFlickering = true;
+    }
+}
