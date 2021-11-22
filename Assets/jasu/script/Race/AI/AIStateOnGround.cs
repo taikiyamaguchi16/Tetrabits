1a930efd0e5998e3302c9777040d7b645af7f873
KentaOishi <justicekenta@i.softbank.jp>
2021-11-22 17:01:30 +0900

    [update]Layer追加、レースゲームのCPU処理追加

---
+++ Assets/jasu/script/Race/AI/AIStateOnGround.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIStateOnGround : AIState
+{
+    [SerializeField]
+    AISensorDistanceTarget sensorDistanceTarget;
+
+    [SerializeField]
+    MoveBetweenLane moveBetweenLane;
+
+    [SerializeField]
+    MoveBetweenLane playerMoveBetweenLane;
+
+    [SerializeField]
+    DirtSplashSpawn dirtSplashSpawn;
+
+    [SerializeField]
+    ColliderSensor colliderSensorFront = null;
+
+    [SerializeField]
+    ColliderSensor colliderSensorBack = null;
+
+    [SerializeField]
+    float correctSeconds = 1f;
+
+    float correctTimer = 0f;
+
+    [SerializeField]
+    float dirtIntervalSeconds = 5f;
+
+    float dirtIntervalTimer = 0f;
+
+    bool canBeDirt = true;
+
+    public override void StateStart()
+    {
+
+    }
+
+    public override void StateUpdate()
+    {
+        if (sensorDistanceTarget.sensorActive && moveBetweenLane.belongingLaneId != playerMoveBetweenLane.belongingLaneId)
+        {
+            correctTimer += Time.deltaTime;
+            if(correctTimer > correctSeconds)
+            {
+                correctTimer = 0f;
+                moveBetweenLane.SetMoveLane(playerMoveBetweenLane.belongingLaneId);
+            }
+        }
+
+        // 泥掛け
+        dirtIntervalTimer += Time.deltaTime;
+        if(dirtIntervalTimer > dirtIntervalSeconds)
+        {
+            canBeDirt = true;
+        }
+
+        if (moveBetweenLane.belongingLaneId == playerMoveBetweenLane.belongingLaneId && sensorDistanceTarget.sensorActive && canBeDirt)
+        {
+            canBeDirt = false;
+            dirtIntervalTimer = 0f;
+
+            dirtSplashSpawn.dirtSplashFlag = true;
+        }
+    }
+
+    public override bool CheckShiftCondition()
+    {
+        if ((colliderSensorFront.GetExistInCollider() ||
+            colliderSensorBack.GetExistInCollider()))
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/jasu/script/StateAI/StateAIManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class StateToName : Serialize.KeyAndValue<string, AIState>
+{
+    public StateToName(string key, AIState value) : base(key, value) {}
+}
+
+
+[System.Serializable]
+public class StateToNameTable : Serialize.TableBase<string, AIState, StateToName> { }
+
+
+public class StateAIManager : MonoBehaviour
+{
+    //[SerializeField]
+    //List<AIState> stateList = new List<AIState>();
+
+    [SerializeField]
+    StateToNameTable stateTable; 
+
+    [SerializeField]
+    AIState activeState = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (activeState != null)
+            activeState.StateStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 遷移条件チェック
+        Dictionary<string, AIState> stateDic = stateTable.GetTable();
+        if(stateDic.Count > 0)
+        {
+            foreach(var state in stateDic)
+            {
+                if (state.Value.CheckShiftCondition())  // 遷移条件を満たしていたら遷移してbreak
+                { 
+                    StateShift(state.Value);
+                    break;
+                }
+            }
+        }
+        
+        // アクティブステートの更新
+        if (activeState != null)
+            activeState.StateUpdate();
+    }
+
+    private void LateUpdate()
+    {
+        if (activeState != null)
+            activeState.StateLateUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        if (activeState != null)
+            activeState.StateFixedUpdate();
+    }
+
+    // ステート遷移
+    public void StateShift(AIState aiState)
+    {
+        // 現在ステートの終了処理
+        if (activeState != null)
+            activeState.StateEnd();
+
+        // 指定のステートに遷移、初期化
+        activeState = aiState;
+        activeState.StateStart();
+    }
+}
