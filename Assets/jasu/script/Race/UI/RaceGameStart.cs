d3ceef695e86f4266105bc696dd2b134200978aa
KentaOishi <justicekenta@i.softbank.jp>
2021-11-22 18:55:38 +0900

    [update]レースゲームのタイトルとステージ１~3作成、遷移処理追加、ビルドセッティングにレースゲームのシーン追加

---
+++ Assets/jasu/script/Race/UI/RaceGameStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceGameStart : MonoBehaviour
+{
+    [SerializeField]
+    SceneObject firstStageScene = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TetraInput.sTetraButton.GetTrigger() && firstStageScene != null)
+        {
+            GameInGameUtil.SwitchGameInGameScene(firstStageScene);
+        }
+    }
+}
