828d3eaa4f8b6f7074cfe589abbb7bcc1fb4f9a8
SatoSho <aries_3042@i.softbank.jp>
2021-11-25 14:28:05 +0900

    [update]タイトルのボタンUI、ルームセレクトのボタンUI

--- Assets/sato/Script/UI/RoomCreateWait.cs
+++ Assets/sato/Script/UI/RoomCreateWait.cs
@@ -7,8 +7,8 @@ using Photon.Realtime;
 public class RoomCreateWait : MonoBehaviourPunCallbacks
 {
     [SerializeField]
-    [Header("カーソル入れる")]
-    GameObject cursor;
+    [Header("ルーム入室などの際消しておきたいオブジェクトを入れる")]
+    GameObject[] DeactiveObjects;
 
     // Start is called before the first frame update
     void Start()
@@ -24,21 +24,33 @@ public class RoomCreateWait : MonoBehaviourPunCallbacks
 
     public override void OnJoinedLobby()
     {
-        cursor.SetActive(true);
+        for(int i = 0; i < DeactiveObjects.Length; i++)
+        {
+            DeactiveObjects[i].SetActive(true);
+        }
     }
 
     public void OnJoiningRoom()
     {
-        cursor.SetActive(false);
+        for (int i = 0; i < DeactiveObjects.Length; i++)
+        {
+            DeactiveObjects[i].SetActive(false);
+        }
     }
 
     public override void OnJoinedRoom()
     {
-        cursor.SetActive(true);
+        for (int i = 0; i < DeactiveObjects.Length; i++)
+        {
+            DeactiveObjects[i].SetActive(true);
+        }
     }
 
     public override void OnJoinRoomFailed(short returnCode, string message)
     {
-        cursor.SetActive(true);
+        for (int i = 0; i < DeactiveObjects.Length; i++)
+        {
+            DeactiveObjects[i].SetActive(true);
+        }
     }
 }
