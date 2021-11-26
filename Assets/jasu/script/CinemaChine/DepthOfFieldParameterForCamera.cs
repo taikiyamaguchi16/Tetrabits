3e06d447cfd1e41dc29c916a17c413aff5d6240a
KentaOishi <justicekenta@i.softbank.jp>
2021-11-27 01:54:18 +0900

    [update]VirtualCameraごとに被写界深度を設定できるようにしました。コンフルエンスのカメラ使い方に使い方追記しました。

---
+++ Assets/jasu/script/CinemaChine/DepthOfFieldParameterForCamera.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthOfFieldParameterForCamera : MonoBehaviour
+{
+    public float forcusDistance;
+
+    public float focalLength;
+}
