4713bbf5623b1a585081a7ff684d531dd447e9f5
SatoSho <aries_3042@i.softbank.jp>
2021-11-29 18:09:41 +0900

    [update]タイトルのコントローラー接続、非接続時の電源起動ボタンを変更

---
+++ Assets/sato/Script/Controller/ControllerDetect.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ControllerDetect : MonoBehaviour
+{
+    string[] CacheJoystickNames;
+    bool ControllerFlag = false;
+    int currentControllerNum = 0, oldControllerNum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CacheJoystickNames = Input.GetJoystickNames();
+
+        for (int i = 0; i < CacheJoystickNames.Length; i++)
+        {
+            if (CacheJoystickNames[i] != "")
+            {
+                currentControllerNum++;
+            }
+        }
+
+        if (currentControllerNum == 0)
+        {
+            Debug.Log("切断");
+            ControllerFlag = false;
+            Debug.Log(ControllerFlag);
+        }
+        else if (currentControllerNum > 0)
+        {
+            Debug.Log("接続");
+            ControllerFlag = true;
+            Debug.Log(ControllerFlag);
+        }
+
+        oldControllerNum = currentControllerNum;
+        currentControllerNum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ConnectionDetect();
+    }
+
+    // 接続検知関数
+    void ConnectionDetect()
+    {
+        CacheJoystickNames = Input.GetJoystickNames();
+
+        for (int i = 0; i < CacheJoystickNames.Length; i++)
+        {
+            if (CacheJoystickNames[i] != "")
+            {
+                currentControllerNum++;
+            }
+        }
+
+        if (currentControllerNum == 0 && oldControllerNum != currentControllerNum)
+        {
+            Debug.Log("切断");
+            ControllerFlag = false;
+            Debug.Log(ControllerFlag);
+        }
+
+        if (currentControllerNum > 0 && oldControllerNum != currentControllerNum)
+        {
+            Debug.Log("接続" + CacheJoystickNames.ToList()[0]);
+            ControllerFlag = true;
+            Debug.Log(ControllerFlag);
+        }
+
+        oldControllerNum = currentControllerNum;
+        currentControllerNum = 0;
+    }
+
+    public bool GetControllerFlag()
+    {
+        return ControllerFlag;
+    }
+}
--- Assets/sato/Script/UI/PowerSwitch.cs
+++ Assets/sato/Script/UI/PowerSwitch.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PowerSwitch : MonoBehaviour
 {
@@ -11,13 +12,18 @@ public class PowerSwitch : MonoBehaviour
     GameObject TitleLogo;
 
     [SerializeField]
+    ControllerDetect controllerDetect;
+
+    TextMeshProUGUI tmp;
+
+    [SerializeField]
     [Header("コントーローラーID")]
     public int controllerID = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tmp = gameObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -35,10 +41,26 @@ public class PowerSwitch : MonoBehaviour
     // 起動
     void PowerOn()
     {
-        // PadのBもしくはキーボードのBで起動
-        if (XInputManager.GetButtonTrigger(controllerID, XButtonType.B) || Input.GetKeyDown(KeyCode.B))
+        // コントローラー接続時
+        if (controllerDetect.GetControllerFlag())
         {
-            isPower = true;
+            tmp.text = "B   電源 ON";
+
+            // PadのB
+            if (XInputManager.GetButtonTrigger(controllerID, XButtonType.B))
+            {
+                isPower = true;
+            }
+        }
+        // コントローラー非接続時
+        else if (!controllerDetect.GetControllerFlag())
+        {
+            tmp.text = "SPACE or ENTER   電源 ON";
+
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+            {
+                isPower = true;
+            }
         }
     }
 }
