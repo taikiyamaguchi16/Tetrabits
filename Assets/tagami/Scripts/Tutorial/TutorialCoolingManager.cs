d829b9fe1cefea217ba79e05dde875885d8548ff
hopehopehope <hope03282000@icloud.com>
2021-12-02 21:15:03 +0900

    [add] 冷却チュートリアルの基盤を作成

---
+++ Assets/tagami/Scripts/Tutorial/TutorialCoolingManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCoolingManager : MonoBehaviour
+{
+    [SerializeField] Trisibo.SceneField nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Tutorial用の炎を出す
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //炎に呼んでもらう
+    public void CompleteCooled()
+    {
+        StartCoroutine(CoCompleteCooled());
+    }
+
+    IEnumerator CoCompleteCooled()
+    {
+
+        yield return null;
+
+        GameInGameUtil.SwitchGameInGameScene(GameInGameUtil.GetSceneNameByBuildIndex(nextScene.BuildIndex));
+    }
+}
