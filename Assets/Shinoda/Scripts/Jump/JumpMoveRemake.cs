ca60535f3a9e42a2d3f23dadf9fa070e79dd9954
nona-nano-na <ohs80645@gmail.com>
2021-12-04 18:27:45 +0900

    [add]JumpGame積み木

---
+++ Assets/Shinoda/Scripts/Jump/JumpMoveRemake.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class JumpMoveRemake : MonoBehaviour
+{
+    Transform parent;
+    GameObject player;
+    GameObject playerFoot;
+
+    Vector3 originPos;
+
+    [SerializeField] float moveX;
+    [SerializeField] float moveY;
+    [SerializeField] float blockSize = 1f;
+    Vector3 targetPos;
+
+    [SerializeField] float moveTime = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GameObject.Find("GameInGame").transform;
+        player = GameObject.Find("JumpMan");
+        playerFoot = player.transform.Find("Foot").gameObject;
+
+        originPos = transform.position;
+        targetPos = new Vector3(originPos.x + (moveX * blockSize), originPos.y + (moveY * blockSize), originPos.z);
+        // 移動床設定
+        this.transform.DOMove(targetPos, moveTime).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == playerFoot)
+        {
+            player.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == playerFoot)
+        {
+            player.transform.parent = parent;
+        }
+    }
+}
