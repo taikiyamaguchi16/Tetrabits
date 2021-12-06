813269bf43acc9741f1c79a9e7c358a5b76a0f72
nona-nano-na <ohs80645@gmail.com>
2021-12-06 17:15:42 +0900

    [update]ジャイロの矢印作りました

---
+++ Assets/Shinoda/Scripts/Gyro/GyroAllowContoroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroAllowContoroller : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject goal;
+    [SerializeField] float distance = 5f;
+    Vector3 workVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (goal == null) goal = GameObject.Find("Goal");
+        //workVec = (goal.transform.position - player.transform.position).normalized;
+        //this.transform.position = (player.transform.position) + (workVec * distance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        workVec = (goal.transform.position - player.transform.position).normalized;
+        this.transform.position = (player.transform.position) + (workVec * distance);
+        this.transform.rotation = Quaternion.FromToRotation(Vector3.up, workVec);
+    }
+}
