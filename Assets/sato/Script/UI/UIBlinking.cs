875b539f9ff18ac8f57b25349f00198aa6334eaa
SatoSho <aries_3042@i.softbank.jp>
2021-12-10 02:26:07 +0900

    [change]assets直下のResourceフォルダにデータ追加しました！！！！！！！！！！！ [add]WaitOnllneScene各プレイヤーの色分け、名前などなどに対応

---
+++ Assets/sato/Script/UI/UIBlinking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class UIBlinking : DOManager
+{
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+
+        text.DOFade(fadeTime, fadeRange).SetEase(easeTypes).SetLoops(loopTimes,loopTypes);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
