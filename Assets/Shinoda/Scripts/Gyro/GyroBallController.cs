42fc8c9436da529b97eb5821082f7ed171ea2e5a
nona-nano-na <ohs80645@gmail.com>
2021-12-13 01:04:03 +0900

    [add]いろいろ

--- Assets/Shinoda/Scripts/Gyro/GyroBallController.cs
+++ Assets/Shinoda/Scripts/Gyro/GyroBallController.cs
@@ -10,6 +10,10 @@ public class GyroBallController : MonoBehaviour
     [SerializeField] float stopTime = 1.0f;
     [SerializeField] float gravityScale = 9.8f;
 
+    [SerializeField] GameObject effectPrefab = null;
+    [SerializeField] Transform effectInstanceTransform;
+    float timeCount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,7 @@ public class GyroBallController : MonoBehaviour
     void Update()
     {
         Vector2 padVec = TetraInput.sTetraPad.GetVector();
+        timeCount += Time.deltaTime;
 
         if (padVec.x > 0)
         {
@@ -31,12 +36,29 @@ public class GyroBallController : MonoBehaviour
             this.transform.Rotate(0.0f, 0.0f, -1.0f * rotateScale);
         }
 
-        if (TetraInput.sTetraButton.GetTrigger())
+        if (TetraInput.sTetraLever.GetPoweredOn())
         {
             rb.velocity = Vector2.zero;
         }
+        //if (TetraInput.sTetraButton.GetTrigger())
+        //{
+        //    rb.velocity = Vector2.zero;
+        //}
 
 
         rb.AddForce(-(this.transform.up) * gravityScale);
+
+        effectInstanceTransform.rotation = Quaternion.FromToRotation(Vector3.right, rb.velocity);
+
+        if (timeCount >3)
+        {
+            InstanceEffect();
+            timeCount = 0;
+        }
+    }
+
+    public void InstanceEffect()
+    {
+        GameObject accelEffect = Instantiate(effectPrefab, effectInstanceTransform);
     }
 }
