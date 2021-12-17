9abd913716b81217bb9fc614428d1670de1d8ab4
hopehopehope <hope03282000@icloud.com>
2021-12-17 19:48:57 +0900

    [add] ケーブルの上にエミッション球が走る機能を追加

---
+++ Assets/tagami/Scripts/GameMain/Device/Cable/CableEmission.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CableEmission : MonoBehaviour
+{
+    //[Header("Waypoints")]
+    [HideInInspector] public List<Transform> waypoints;
+    float totalDistance;
+
+    [Header("Status")]
+    [SerializeField] float emissionSpeed = 1.0f;
+    float emissionLerpSeconds;
+    float emissionLerpTimer;
+
+    private void Start()
+    {
+        //何秒かけて最後まで行くか
+        totalDistance = Generic.Utility.MathfUtil.DistanceWaypoints(waypoints);
+        emissionLerpSeconds = totalDistance / emissionSpeed;
+
+        //位置の初期化
+        transform.position = Generic.Utility.Vector3Util.LerpWaypoints(waypoints, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        //Move
+        emissionLerpTimer += Time.fixedDeltaTime;
+        if (emissionLerpTimer >= emissionLerpSeconds)
+        {
+            emissionLerpTimer = emissionLerpSeconds;
+            Destroy(gameObject);
+        }
+        transform.position = Generic.Utility.Vector3Util.LerpWaypoints(waypoints, emissionLerpTimer / emissionLerpSeconds);
+    }
+}
---
+++ Assets/tagami/Scripts/GameMain/Device/Cable/CableEmissionGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CableEmissionGenerator : MonoBehaviour
+{
+    [Header("Waypoints")]
+    [SerializeField] List<Transform> waypoints;
+
+    [Header("Reference Battery Holder")]
+    [SerializeField] BatteryHolder batteryHolder;
+
+    [Header("Generate")]
+    [SerializeField] GameObject cableEmissionPrefab;
+    [SerializeField] float generateIntervalSeconds = 5.0f;
+    float generateTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (batteryHolder.GetBatterylevel() > 0)
+        {
+            generateTimer += Time.fixedDeltaTime;
+            if (generateTimer >= generateIntervalSeconds)
+            {
+                generateTimer = 0.0f;
+                //生成
+                var obj = Instantiate(cableEmissionPrefab);
+                obj.GetComponent<CableEmission>().waypoints = waypoints;
+            }
+        }
+    }
+}
