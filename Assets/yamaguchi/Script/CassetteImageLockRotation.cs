04e6d6b2417f787c1bcb43bc5d4e6cd2d8c53b34
tai <taikitpmiita@icloud.com>
2021-12-29 18:59:18 +0900

    [fix]UIバグ

---
+++ Assets/yamaguchi/Script/CassetteImageLockRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CassetteImageLockRotation : MonoBehaviour
+{  
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.parent.localScale.x<0f)
+        {
+            this.transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+        else
+        {
+            this.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+    }
+}
--- Assets/yamaguchi/Script/Item/LockRotation.cs
+++ Assets/yamaguchi/Script/Item/LockRotation.cs
@@ -8,38 +8,42 @@ public class LockRotation : MonoBehaviour
     //どれだけ上に表示するか
     [SerializeField]
     private float upPosition;
+
+    Vector3 def;
+
+    [SerializeField]
+    GameObject targetObj;
     // Start is called before the first frame update
     // Update is called once per frame
 
     private void Awake()
     {       
-        defaultScale = transform.lossyScale;
+        defaultScale = targetObj.transform.lossyScale;
+        def = targetObj.transform.localRotation.eulerAngles;
     }
     void Update()
     {
-        if (this.transform.parent.localScale.x > 0f)
-        {
-            this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        }
-        else
-        {
-            this.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-        }
-        Vector3 lossScale = transform.lossyScale;
-        Vector3 localScale = transform.localScale;
+        Vector3 _parent = transform.transform.localRotation.eulerAngles;
 
-        transform.localScale = new Vector3(
+        //修正箇所
+        targetObj.transform.localRotation = Quaternion.Euler(def - _parent);
+
+
+        Vector3 lossScale = targetObj.transform.lossyScale;
+        Vector3 localScale = targetObj.transform.localScale;
+
+        targetObj.transform.localScale = new Vector3(
                 localScale.x / lossScale.x * defaultScale.x,
                 localScale.y / lossScale.y * defaultScale.y,
-                defaultScale.z);
+                localScale.z / lossScale.z * defaultScale.z);
 
-        Vector3 keepPos = this.transform.parent.position;
+        Vector3 keepPos = targetObj.transform.parent.position;
 
         keepPos.y += upPosition;
-        this.transform.position = keepPos;
+        targetObj.transform.position = keepPos;
 
         Vector3 p = Camera.main.transform.position;
-        p.y = transform.position.y;
-        transform.LookAt(p);
+        p.y = targetObj.transform.position.y;
+        targetObj.transform.LookAt(p);
     }
 }
